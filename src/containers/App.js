bcc4449cfa21cfb9cb10df35e6f80a2d76aedbfc
Nique50 <derrick.washington36@gmail.com>
2022-12-16 20:00:31 -0600

    new file for errors

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "../containers/App.css";
 import Scroll from "../components/Scroll";
-
+import ErrorBoundary from '../components/ErrorBoundary';
 // Two states that App component owns
 class App extends Component {
   constructor() {
@@ -35,8 +35,10 @@ class App extends Component {
       <div className="tc">
         <h1 className="f2">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+              </ErrorBoundary>
         </Scroll>
       </div>
     );
